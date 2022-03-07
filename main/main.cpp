72b475338ddab95ab460ee3a6a4702742a77ad3a
paperdev <jornveken2@gmail.com>
2022-03-07 13:42:45 +0100

    Added app and Arduino initialization

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,13 @@
+#include <Arduino.h>
 #include "app/app.hpp"
 
 extern "C" void app_main();
 
 void app_main() {
+    initArduino();
 
+    auto app = App();
+    app.start();
+
+    while (true); //hang
 }
