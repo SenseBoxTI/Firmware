bcca3c993eec4edc02fc9f2751548cb61fd8746f
controlol <laluckyluc7@gmail.com>
2022-04-13 21:37:27 +0200

    remove unnecessary include

--- main/app/sdcard/sd.hpp
+++ main/app/sdcard/sd.hpp
@@ -1,13 +1,11 @@
 #pragma once
 #include "sdmmc_cmd.h"
-#include "file.hpp"
 
 #define MOUNT_POINT "/sdcard"
 
 class CSd {
     sdmmc_card_t * m_Card;
     public:
-        
         static CSd& getInstance();
         void mInit();
 };
