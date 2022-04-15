5a48d58816fd8a4dcbcaa06b901d729ea0aa42a0
RichardvanBrum <rvanbrummelen@live.nl>
2022-04-15 10:33:16 +0200

    Format time using raw values instead of strftime

--- main/app/logger/log.cpp
+++ main/app/logger/log.cpp
@@ -37,16 +37,14 @@ void CLog::mWriteLog(const char* scope, const std::string& aText, LogType aType)
     time (&rawtime);
     struct tm timeinfo = *localtime(&rawtime);
 
-    char timestr[26];
-    memset(timestr, 0, sizeof(timestr));
-    strftime(timestr, sizeof(timestr), "[ %d/%m/%Y | %H:%M:%S ]", &timeinfo);
+    std::string time = string_vformat("[ %02d/%02d/%4d | %02d:%02d:%02d ]", timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
     // print to serial
-    std::printf("%c %s (%s) %s\n", logIds[uint8_t(aType)], timestr, scope, aText.c_str());
+    std::printf("%c %s (%s) %s\n", logIds[uint8_t(aType)], time.c_str(), scope, aText.c_str());
     
     // print to logfile
     try {
         m_Log.mAppend(string_vformat("%c ", logIds[uint8_t(aType)]));
-        m_Log.mAppend(std::string(timestr, sizeof(timestr) - 1));
+        m_Log.mAppend(time);
         m_Log.mAppend(string_vformat(" (%s) ", scope));
         m_Log.mAppend(aText.c_str());
         m_Log.mAppend("\n");
@@ -56,7 +54,7 @@ void CLog::mWriteLog(const char* scope, const std::string& aText, LogType aType)
     }
     catch (const std::runtime_error& e) {
         // Removed because clutter on serial log
-        //std::printf("E %s (CLog.mWriteLog) Failed to write log to file: %s\n", timestr, e.what());
+        //std::printf("E %s (CLog.mWriteLog) Failed to write log to file: %s\n", time.c_str(), e.what());
     }
 }
 
