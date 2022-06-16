56e31816d305cbf6e06c3e80614bb13682f62244
RichardvanBrum <98885494+RichardvanBrum@users.noreply.github.com>
2022-06-16 10:36:09 +0200

    Log error instead of emplacing in output, remove eco2 comment

--- main/app/sensors/impl/vocsensor.cpp
+++ main/app/sensors/impl/vocsensor.cpp
@@ -1,37 +1,36 @@
 #include "vocsensor.hpp"
 #include "scdsensor.hpp"
 #include <Adafruit_CCS811.h>
+#include <logscope.hpp>
 
+static CLogScope logger{"vocsensor"};
 
 Adafruit_CCS811 ccs = Adafruit_CCS811();
 
 CVocSensor::CVocSensor(std::string aName, CScdSensor& arScdSensor)
 :   CSensor(aName),
-    mr_ScdSensor(arScdSensor){
+    mr_ScdSensor(arScdSensor) {
 
 }
 
-SensorOutput CVocSensor::m_MeasureCallback(){
+SensorOutput CVocSensor::m_MeasureCallback() {
     SensorOutput output;
     uint8_t error;
     ccs.setEnvironmentalData(mr_ScdSensor.get_LastRelative_humidity(), mr_ScdSensor.get_LastTemperature());
-    if ((error = ccs.readData()) == 0){
-        // output.emplace("CO2", std::to_string(ccs.geteCO2()));
+    if ((error = ccs.readData()) == 0) {
         output.emplace("TVOC ppb", std::to_string(ccs.getTVOC()));
+    } else {
+        logger.mError("TVOC ppb read fail | error code: %d", error);
     }
-    else{
-        // output.emplace("VOC sensor read (CCS811) error","");
-        output.emplace("TVOC ppb read fail | error code", std::to_string(error));
-    }
+
     return output;
 }
 
-CSensorStatus CVocSensor::m_InitCallback(){
-    if(ccs.begin()){
+CSensorStatus CVocSensor::m_InitCallback() {
+    if(ccs.begin()) {
         ccs.setDriveMode(CCS811_DRIVE_MODE_10SEC);//sample rate
         return CSensorStatus::Ok();
-    }
-    else{
+    } else {
         return CSensorStatus::Error("Cant connect VOC sensor I2C!");
     }
 }
