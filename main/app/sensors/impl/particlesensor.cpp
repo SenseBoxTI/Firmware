9ed1ff6c8b8456e351f20a7a4d14a9164f02a6a4
RichardvanBrum <98885494+RichardvanBrum@users.noreply.github.com>
2022-06-16 10:39:26 +0200

    Log error instead of emplacing in output

--- main/app/sensors/impl/particlesensor.cpp
+++ main/app/sensors/impl/particlesensor.cpp
@@ -1,28 +1,26 @@
 #include "particlesensor.hpp"
 #include <Adafruit_PM25AQI.h>
+#include <logscope.hpp>
+
+static CLogScope logger{"particlesensor"};
 
 Adafruit_PM25AQI pmsa = Adafruit_PM25AQI();
 
-SensorOutput CParticleSensor::m_MeasureCallback(){
+SensorOutput CParticleSensor::m_MeasureCallback() {
     SensorOutput output;
     PM25_AQI_Data data;
-    if(pmsa.read(&data)){
-        // output.emplace("PM 1.0", std::to_string(data.pm10_standard));
+    if (pmsa.read(&data)) {
         output.emplace("PM 2.5", std::to_string(data.pm25_standard));
-
-    }
-    else{
-        output.emplace("PM 2.5"," Read fail");
+    } else {
+        logger.mError("Read fail");
     }
     return output;
 }
 
-CSensorStatus CParticleSensor::m_InitCallback(){
-    if(pmsa.begin_I2C()){
+CSensorStatus CParticleSensor::m_InitCallback() {
+    if (pmsa.begin_I2C()) {
         return CSensorStatus::Ok();
-        
-    }
-    else{
+    } else {
         return CSensorStatus::Error("Cant connect particle sensor I2C!");
     }
 }
