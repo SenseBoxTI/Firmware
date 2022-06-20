edcec0e8261e35682577312cd84e8b82665f639b
controlol <laluckyluc7@gmail.com>
2022-06-20 12:35:43 +0200

    update wifi reinit for softrestart

--- main/app/Wifi/wifi.cpp
+++ main/app/Wifi/wifi.cpp
@@ -83,26 +83,32 @@ void CWifi::mInitWifi(const WifiCredentials& aConfig) {
 
     if (mCredentials.ssid.size() > 32) throw std::runtime_error("SSID is longer than 32 characters");
     if (enterprise && mCredentials.password.size() > 64) throw std::runtime_error("Password is longer than 64 characters");
-    WIFI_THROW_ON_ERROR(esp_netif_init());
-    m_EventGroup = xEventGroupCreate();
-    WIFI_THROW_ON_ERROR(esp_event_loop_create_default());
-    m_StaNetif = esp_netif_create_default_wifi_sta();
-    if (!m_StaNetif) throw std::runtime_error("WTF WIFI");
+
+    if (m_StaNetif == nullptr) {
+        WIFI_THROW_ON_ERROR(esp_netif_init());
+        m_EventGroup = xEventGroupCreate();
+        WIFI_THROW_ON_ERROR(esp_event_loop_create_default());
+        m_StaNetif = esp_netif_create_default_wifi_sta();
+        if (!m_StaNetif) throw std::runtime_error("WTF WIFI");
+        WIFI_THROW_ON_ERROR( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &CWifi::m_EventHandler, NULL) );
+        WIFI_THROW_ON_ERROR( esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &CWifi::m_EventHandler, NULL) );
+    }
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     WIFI_THROW_ON_ERROR( esp_wifi_init(&cfg) );
-    WIFI_THROW_ON_ERROR( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &CWifi::m_EventHandler, NULL) );
-    WIFI_THROW_ON_ERROR( esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &CWifi::m_EventHandler, NULL) );
     WIFI_THROW_ON_ERROR( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+
     wifi_config_t wifi_config = {};
     memcpy(wifi_config.sta.ssid, mCredentials.ssid.c_str(), mCredentials.ssid.size());
     if (!enterprise) {
         memcpy(wifi_config.sta.password, mCredentials.password.c_str(), mCredentials.password.size());
     }
+
     wifi_config.sta.pmf_cfg.required = false;
     logger.mInfo("Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
     WIFI_THROW_ON_ERROR( esp_wifi_set_mode(WIFI_MODE_STA) );
     WIFI_THROW_ON_ERROR( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+
     if (enterprise) {
         WIFI_THROW_ON_ERROR( esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)mCredentials.eapId.c_str(), mCredentials.eapId.size()) );
 
@@ -110,6 +116,7 @@ void CWifi::mInitWifi(const WifiCredentials& aConfig) {
         WIFI_THROW_ON_ERROR( esp_wifi_sta_wpa2_ent_set_password((uint8_t *)mCredentials.password.c_str(), mCredentials.password.size()) );
         WIFI_THROW_ON_ERROR( esp_wifi_sta_wpa2_ent_enable() );
     }
+
     WIFI_THROW_ON_ERROR( esp_wifi_start() );
 
     logger.mDebug("Initializing WiFi");
@@ -141,7 +148,13 @@ void CWifi::mDisconnect() {
 }
 
 void CWifi::mDeinit() {
-    esp_wifi_disconnect();
-    esp_wifi_stop();
-    esp_wifi_deinit();
+    logger.mDebug("wifi_init_sta stop sta");
+    ESP_ERROR_CHECK(esp_wifi_stop());
+
+    logger.mDebug("wifi_init_sta deinit sta");
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+
+    m_ConnectCbs.clear();
+    m_DisconnectCbs.clear();
+    logger.mDebug("wifi succesfully deinited");
 }
